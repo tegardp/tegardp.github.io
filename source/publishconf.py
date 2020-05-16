227019b85768e2381d59aa582742c07f6b5ed87a
tegardp <tegar1195@gmail.com>
2020-05-16 17:44:17 +0900

    artikel: perjalanan menuju kebebasan financial pns

--- source/publishconf.py
+++ source/publishconf.py
@@ -21,5 +21,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "tegardp"
+GOOGLE_ANALYTICS = "UA-134434570-2"
